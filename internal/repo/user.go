4b7dc86ea9e6a6e5816e5fac68ec8be9ecbd57bf
moxirboy <mox777boy@gmail.com>
2024-01-17 19:58:59 +0500

    added user repo interface

--- internal/repo/user.go
+++ internal/repo/user.go
@@ -1,5 +1,14 @@
 package repo
 
+import (
+	"arch/internal/models"
+	"context"
+)
+
 type IUserRepository interface {
-	
+	CreateUser(ctx context.Context, user *models.User) (err error)
+	ExistUser(ctx context.Context, email string) (exist bool, err error)
+	GetByEmail(ctx context.Context, email string) (*models.User, error)
+	UpdateIsVerified(ctx context.Context, id string) error
+	Delete(ctx context.Context, id string) error
 }
